bbd02db8c819e8ef5c39b0cfb1a72d546031e7a1
Yobi Bina Setiawan <yobi.studio@gmail.com>
2022-04-12 07:16:26 +0700

    form input

--- lib/catalog/form_input.dart
+++ lib/catalog/form_input.dart
@@ -13,17 +13,87 @@ class MyFormInput extends StatelessWidget {
           Container(
             padding: const EdgeInsets.all(15),
             color: Colors.black12,
-            child: const Text('Contoh Form Input'),
+            child: const Text('Contoh TextField'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(15),
+            child: const TextField(
+              decoration: InputDecoration(
+                hintText: 'Place Holder',
+                labelText: 'Label Text',
+              ),
+            ),
           ),
           Container(
             padding: const EdgeInsets.all(15),
             color: Colors.black12,
-            child: const Text('Contoh Form Input Outline'),
+            child: const Text('Contoh TextField Max Length'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(15),
+            child: const TextField(
+              maxLength: 5,
+              decoration: InputDecoration(
+                hintText: 'TextField with maxLength',
+              ),
+            ),
           ),
           Container(
             padding: const EdgeInsets.all(15),
             color: Colors.black12,
-            child: const Text('Contoh Form Input Password'),
+            child: const Text('Contoh TextField Multiline'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(15),
+            child: const TextField(
+              maxLines: 5,
+              decoration: InputDecoration(
+                hintText: 'TextField with multiline',
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(15),
+            color: Colors.black12,
+            child: const Text('Contoh TextField Outline'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(15),
+            child: const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'TextField OutlineInputBorder',
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(15),
+            color: Colors.black12,
+            child: const Text('Contoh TextField BorderLess'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(15),
+            child: const TextField(
+              decoration: InputDecoration.collapsed( 
+                hintText: 'Contoh TextField BorderLess',
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(15),
+            color: Colors.black12,
+            child: const Text('Contoh TextField Password'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(15),
+            child: const TextField(
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration: InputDecoration(
+                hintText: 'TextField obscureText true',
+              ),
+            ),
           ),
         ]));
   }
