ac5faaa0ab1387eeab8caec4442215e05dc18dcb
Yobi Bina Setiawan <yobi.studio@gmail.com>
2022-04-23 05:42:45 +0700

    add widget navbar bottom

--- lib/catalog/navbar_bottom.dart
+++ lib/catalog/navbar_bottom.dart
@@ -18,12 +18,15 @@ class _BottomNavigationBarExampleState
       const Center(child: Icon(Icons.alarm, size: 64.0, color: Colors.cyan)),
       const Center(child: Icon(Icons.forum, size: 64.0, color: Colors.blue)),
     ];
+
     final _kBottmonNavBarItems = <BottomNavigationBarItem>[
       const BottomNavigationBarItem(icon: Icon(Icons.cloud), label: 'Tab1'),
       const BottomNavigationBarItem(icon: Icon(Icons.alarm), label: 'Tab2'),
       const BottomNavigationBarItem(icon: Icon(Icons.forum), label: 'Tab3'),
     ];
+
     assert(_kTabPages.length == _kBottmonNavBarItems.length);
+
     final bottomNavBar = BottomNavigationBar(
       items: _kBottmonNavBarItems,
       currentIndex: _currentTabIndex,
